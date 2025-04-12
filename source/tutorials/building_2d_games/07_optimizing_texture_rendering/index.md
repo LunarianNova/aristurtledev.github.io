48957c4a63fb4344afdee4480914b084bf36312b
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-04-12 12:07:53 -0400

    Sync with rewrite

--- source/tutorials/building_2d_games/07_optimizing_texture_rendering/index.md
+++ source/tutorials/building_2d_games/07_optimizing_texture_rendering/index.md
@@ -183,7 +183,9 @@ The key changes in this implementation are:
     - Created a `TextureAtlas` with the atlas texture.
     - Added regions for both the slime and the bat.
     - Retrieved the regions using their names.
-4. Updated [**Draw**](xref:Microsoft.Xna.Framework.Game.Draw(Microsoft.Xna.Framework.GameTime)) to render both sprites, using the slime's `Width` property to position the bat.
+4. Updated [**Draw**](xref:Microsoft.Xna.Framework.Game.Draw(Microsoft.Xna.Framework.GameTime)) to:
+   - Draw the slime at a scale factor of 4.
+   - Draw the bat 10 pixels to the right of the bat based on the slime's `Width` property, at a scale of 4
 
 Running the game now shows both sprites in the upper-left corner:
 
--- source/tutorials/building_2d_games/17_scenes/snippets/gamescene.cs
+++ source/tutorials/building_2d_games/17_scenes/snippets/gamescene.cs
@@ -75,23 +75,23 @@ public class GameScene : Scene
         Rectangle screenBounds = Core.GraphicsDevice.PresentationParameters.Bounds;
 
         _roomBounds = new Rectangle(
-            _tilemap.TileSize,
-            _tilemap.TileSize,
-            screenBounds.Width - _tilemap.TileSize * 2,
-            screenBounds.Height - _tilemap.TileSize * 2
-        );
+             (int)_tilemap.TileWidth,
+             (int)_tilemap.TileHeight,
+             screenBounds.Width - (int)_tilemap.TileWidth * 2,
+             screenBounds.Height - (int)_tilemap.TileHeight * 2
+         );
 
         // Initial slime position will be the center tile of the tile map.
         int centerRow = _tilemap.Rows / 2;
         int centerColumn = _tilemap.Columns / 2;
-        _slimePosition = new Vector2(centerColumn, centerRow) * _tilemap.TileSize;
+        _slimePosition = new Vector2(centerColumn * _tilemap.TileWidth, centerRow * _tilemap.TileHeight);
 
         // Initial bat position will the in the top left corner of the room
         _batPosition = new Vector2(_roomBounds.Left, _roomBounds.Top);
 
         // Set the position of the score text to align to the left edge of the
         // room bounds, and to vertically be at the center of the first tile.
-        _scoreTextPosition = new Vector2(_roomBounds.Left, _tilemap.TileSize * 0.5f);
+        _scoreTextPosition = new Vector2(_roomBounds.Left, _tilemap.TileHeight * 0.5f);
 
         // Set the origin of the text so it's left-centered.
         float scoreTextYOrigin = _font.MeasureString("Score").Y * 0.5f;
@@ -110,12 +110,15 @@ public class GameScene : Scene
 
         // Create the slime animated sprite from the atlas.
         _slime = atlas.CreateAnimatedSprite("slime-animation");
+        _slime.Scale = new Vector2(4.0f, 4.0f);
 
         // Create the bat animated sprite from the atlas.
         _bat = atlas.CreateAnimatedSprite("bat-animation");
+        _bat.Scale = new Vector2(4.0f, 4.0f);
 
-        // Load the tilemap from the XML configuration file.
+        // Create the tilemap from the XML configuration file.
         _tilemap = Tilemap.FromFile(Content, "images/tilemap-definition.xml");
+        _tilemap.Scale = new Vector2(4.0f, 4.0f);
 
         // Load the bounce sound effect
         _bounceSoundEffect = Content.Load<SoundEffect>("audio/bounce");
@@ -124,7 +127,7 @@ public class GameScene : Scene
         _collectSoundEffect = Content.Load<SoundEffect>("audio/collect");
 
         // Load the font
-        _font = Core.Content.Load<SpriteFont>("fonts/gameFont");
+        _font = Core.Content.Load<SpriteFont>("fonts/04B_30");
     }
     #endregion
 
