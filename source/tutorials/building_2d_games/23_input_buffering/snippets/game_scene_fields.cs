d7c611d25337b2e448204dabc59bbcebe74c8a67
Christopher Whitley <aristurtledev@gmail.com>
2025-04-30 23:34:09 -0400

    Sync

--- source/tutorials/building_2d_games/23_input_buffering/snippets/game_scene_fields.cs
+++
@@ -1,5 +0,0 @@
-// Buffer to queue inputs input by player during input polling.
-private Queue<Vector2> _inputBuffer;
-
-// The maximum size of the buffer queue.
-private const int MAX_BUFFER_SIZE = 2;
